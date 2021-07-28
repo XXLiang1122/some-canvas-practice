9cd2f13336d35545e0cbca85c104d892f4950f69
xiexueliang <xiexueliang@doupai.cc>
2021-07-28 16:29:45 +0800

    demo3

--- src/components/demo3/Index.vue
+++ src/components/demo3/Index.vue
@@ -15,10 +15,90 @@ export default defineComponent({
 
     onMounted (() => {
       init()
+      drawClock()
     })
 
     const init = () => {
       ctx = canvas.value?.getContext('2d') as CanvasRenderingContext2D
+      ctx.translate(200, 200)
+    }
+
+    const drawClock = () => {
+      ctx.clearRect(-200, -200, 400, 400)
+      /** 圆盘 */
+      ctx.lineWidth = 4
+      ctx.strokeStyle = '#000000'
+      ctx.fillStyle = '#000000'
+      ctx.beginPath()
+      ctx.arc(0, 0, 180, 0, Math.PI * 2 * 180)
+      ctx.stroke()
+
+      /** 时针刻度 */
+      ctx.save()
+      ctx.lineWidth = 3
+      for (let i = 0; i < 12; i++) {
+        ctx.beginPath()
+        ctx.moveTo(180, 0)
+        ctx.lineTo(160, 0)
+        ctx.stroke()
+        ctx.rotate(Math.PI * 2 / 12)
+      }
+      ctx.restore()
+
+      /** 分针刻度 */
+      ctx.save()
+      ctx.lineWidth = 1
+      for (let i = 0; i < 60; i++) {
+        ctx.beginPath()
+        ctx.moveTo(180, 0)
+        ctx.lineTo(170, 0)
+        ctx.stroke()
+        ctx.rotate(Math.PI * 2 / 60)
+      }
+      ctx.restore()
+
+      const now = new Date()
+      const h = now.getHours()
+      const m = now.getMinutes()
+      const s = now.getSeconds()
+
+      /** 时针 */
+      ctx.save()
+      ctx.lineWidth = 8
+      ctx.rotate(Math.PI * 2 / 12 * (Math.abs(h - 12)))
+      ctx.beginPath()
+      ctx.moveTo(0, 30)
+      ctx.lineTo(0, -100)
+      ctx.stroke()
+      ctx.restore()
+
+      /** 分针 */
+      ctx.save()
+      ctx.lineWidth = 4
+      ctx.rotate(Math.PI * 2 / 60 * m)
+      ctx.beginPath()
+      ctx.moveTo(0, 30)
+      ctx.lineTo(0, -140)
+      ctx.stroke()
+      ctx.restore()
+
+      /** 秒针 */
+      ctx.save()
+      ctx.lineWidth = 2
+      ctx.strokeStyle = '#ff0000'
+      ctx.rotate(Math.PI * 2 / 60 * s)
+      ctx.beginPath()
+      ctx.moveTo(0, 30)
+      ctx.lineTo(0, -140)
+      ctx.stroke()
+      ctx.restore()
+
+      /** 中心点 */
+      ctx.beginPath()
+      ctx.arc(0, 0, 10, 0, Math.PI * 2 * 10)
+      ctx.fill()
+
+      window.requestAnimationFrame(drawClock)
     }
 
     return {
